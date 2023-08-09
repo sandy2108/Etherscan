6f4c56f73a9695de2d40e64565a30b8b7ff6ebd3
sandy2108 <dhonisanjay116@gmail.com>
2023-08-09 15:59:07 +0530

    Add a tag to the Etherscan logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import logo from '../Assets/logo-etherscan.svg';
-import { navItems } from '../constants';
-import { CgMenu } from 'react-icons/cg';
+import React, { useState } from "react";
+import { navItems } from "../constants";
+import { CgMenu } from "react-icons/cg";
+import { EtherscanIcon } from "../Icons/EtherscanIcon";
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
@@ -14,18 +14,20 @@ const Navbar = () => {
     <div className="w-full bg-slate-100">
       <div className="max-w-[1240px] mx-auto px-4 py-2">
         <div className="flex items-center justify-between">
-          <div className="text-white">
-            <img src={logo} alt="logo" className="h-[30px] cursor-pointer" />
+          <div className="text-white cursor-pointer">
+            <a href="/"><EtherscanIcon className="max-w-[150px] max-h-[40px]" /></a>
           </div>
           <div className="hidden md:flex">
             {navItems.map((item, index) => (
               <a href={item.link} key={index} className="mx-2">
-                <h1 className='text-black hover:text-[#0784c3]'>{item.title}</h1>
+                <h1 className="text-black hover:text-[#0784c3]">
+                  {item.title}
+                </h1>
               </a>
             ))}
           </div>
           <div className="flex md:hidden rounded-lg p-2 border-2 border-slate-200">
-            <CgMenu onClick={toggleMenu} size={20} className='cursor-pointer' />
+            <CgMenu onClick={toggleMenu} size={20} className="cursor-pointer" />
           </div>
         </div>
         {openMenu && (
@@ -36,7 +38,9 @@ const Navbar = () => {
                 key={index}
                 className="flex flex-col py-1 px-4 text-[14px] leading-[22px] text-center text-gray-700 items-start hover:bg-gray-100"
               >
-                <h1 className='text-black hover:text-[#0784c3]'>{item.title}</h1>
+                <h1 className="text-black hover:text-[#0784c3]">
+                  {item.title}
+                </h1>
               </a>
             ))}
           </div>
