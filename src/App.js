738c70ef3e9a0d85984dfe308942b4084dcb46f1
sandy2108 <dhonisanjay116@gmail.com>
2023-08-12 23:21:51 +0530

    Added section for block & transaction component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import SearchBar from './components/SearchBar';
 import Data from './components/Data';
+import LatestTransaction from './components/LatestTransaction';
+import LatestBlock from './components/LatestBlock';
+import Footer from './components/Footer';
 
 // Refer to the README doc for more information about using API
 // keys in client-side code. You should never do this in production
@@ -38,7 +41,14 @@ function App() {
         <Navbar/>
         <SearchBar/>
         <Data/>
-       
+        <section className="max-w-[1340px] mx-auto px-4 bg-[#fbfbfe]">
+          <div className='grid md:grid-cols-2 gap-3 mb-10'>
+            <LatestBlock/>
+            <LatestTransaction/>
+          </div>
+        </section>
+        <Footer/>
+
       </>
   ) 
   }
