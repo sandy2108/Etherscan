94e6a537d6da4bbbcffb2c166b59b0d51c8b870e
sandy2108 <dhonisanjay116@gmail.com>
2023-08-13 13:32:03 +0530

    Added FooterItems Schemea

--- src/constants/index.js
+++ src/constants/index.js
@@ -28,3 +28,89 @@ export const navItems = [
         link:'/'
     }
 ]
+
+export const footerItems = [
+    {
+        title:'Company',
+        contents: [
+            {
+                subtitle:'About Us',
+                link:'https://etherscan.io/aboutus',
+            },
+            {
+                subtitle:'Brand Assets',
+                link:'https://etherscan.io/brandassets',
+            },
+            {
+                subtitle:'Contact Us',
+                link:'https://etherscan.io/contactus',
+            },
+            {
+                subtitle:'Career',
+                link:'https://etherscan.io/careers',
+            },
+            {
+                subtitle:'Terms of Service',
+                link:'https://etherscan.io/terms',
+            },
+            {
+                subtitle:'Bug Bounty',
+                link:'https://etherscan.io/bugbounty',
+            },
+        ],
+    },
+    {
+        title:'Community',
+        contents: [
+            {
+                subtitle:'API Documentation',
+                link:'https://docs.etherscan.io/'
+            },
+            {
+                subtitle:'Knowledge Base',
+                link:'https://info.etherscan.com/',
+            },
+            {
+                subtitle:"Network Status",
+                link:'https://etherscan.freshstatus.io/',
+            },
+            {
+                subtitle:'Newsletters',
+                link:'https://info.etherscan.com/etherscan-newsletters/',
+            },
+            {
+                subtitle:'Disqus Comments',
+                link:'https://etherscan.io/comments',
+            },
+        ],
+    },
+    {
+        title:'Products & Services',
+        contents:[
+            {
+                subtitle:'Advertise',
+                link:'https://etherscan.io/contactusadvertise',
+            },
+            {
+                subtitle:'Explorer-as-a-Service (EaaS)',
+                link:'https://etherscan.io/eaas',
+            },
+            {
+                subtitle:'API Plans',
+                link:'https://etherscan.io/apis',
+            },
+            {
+                subtitle:'Priority Support',
+                link:'https://etherscan.io/prioritysupport',
+            },
+            {
+                subtitle:'Blockscan',
+                link:'https://blockscan.com/',
+            },
+            {
+                subtitle:'Blockscan Chat',
+                link:'https://chat.blockscan.com/start'
+            },
+        ],
+    },
+]
