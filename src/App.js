2436cf4aecbac769d1481dd7b6c9cbe11a7c4141
sandy2108 <dhonisanjay116@gmail.com>
2023-08-14 15:22:28 +0530

    Removed unncessary datas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
 
 import './App.css';
 import Navbar from './components/Navbar';
@@ -9,46 +7,21 @@ import LatestTransaction from './components/LatestTransaction';
 import LatestBlock from './components/LatestBlock';
 import Footer from './components/Footer';
 
-// Refer to the README doc for more information about using API
-// keys in client-side code. You should never do this in production
-// level code.
-const settings = {
-  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
-  network: Network.ETH_MAINNET,
-};
-
-
-// In this week's lessons we used ethers.js. Here we are using the
-// Alchemy SDK is an umbrella library with several different packages.
-//
-// You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
-const alchemy = new Alchemy(settings);
 
 function App() {
-  const [blockNumber, setBlockNumber] = useState();
-
-  useEffect(() => {
-    async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
-    }
-
-    getBlockNumber();
-  });
-
   return (
       <>
         <Navbar/>
         <SearchBar/>
         <Data/>
-        <section className="max-w-[1340px] mx-auto px-4 bg-[#fbfbfe]">
+        <section className="max-w-[1380px] mx-auto px-4 bg-[#fbfbfe]">
           <div className='grid md:grid-cols-2 gap-3 mb-10'>
             <LatestBlock/>
             <LatestTransaction/>
           </div>
         </section>
         <Footer/>
-
+       
       </>
   ) 
   }
