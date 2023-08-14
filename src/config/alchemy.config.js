9655395122396ad9c3690f1fad98f6795a6f8312
sandy2108 <dhonisanjay116@gmail.com>
2023-08-14 22:17:47 +0530

    Alchemy API config

---
+++ src/config/alchemy.config.js
@@ -0,0 +1,9 @@
+
+import { Alchemy ,Network } from "alchemy-sdk";
+
+const settings = {
+    apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+    network: Network.ETH_MAINNET,
+}
+
+export const alchemy = new Alchemy(settings);
